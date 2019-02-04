322c841970361a0d00a0f1dfc969c95294bb065d
Alwertus <alwertus@yandex.ru>
2019-02-04 15:22:14 +0300

    Initial commit

---
+++ src/main/java/ru/alwertus/webserver/Main.java
@@ -0,0 +1,20 @@
+package ru.alwertus.webserver;
+
+import freemarker.ext.servlet.ServletContextHashModel;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Frontend frontend = new Frontend();
+
+        Server server = new Server(8080);
+        ServletContextHashModel context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(frontend), "/authform");
+
+        server.start();
+        server.join();
+    }
+}
